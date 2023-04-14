99d0b78ad23c2785b8cc566a09ae69dc00ad4cd1
Christopher Richard <noobguideforyou@gmail.com>
2023-04-15 04:36:38 +0700

    second commit

--- TubesDaspro.py
+++ TubesDaspro.py
@@ -11,11 +11,14 @@ userFile : str = [[""] for i in range(101)]
 bahanBangunan : str = [[""] for i in range(101)]
 daftarCandi : str = [[""] for i in range(101)]
 sedangBerjalan : bool = True
-
+seed : int = 0
+totalBatu : int = 0
+totalPasir : int = 0
+totalAir : int = 0
 
 #Kumpulan Fungsi 
 
-def panjangArray(namaFile): 
+def panjangFile(namaFile): 
     f = open(namaFile, "r")
     i = 0
     t = "test"
@@ -23,6 +26,7 @@ def panjangArray(namaFile):
         t = f.readline()
         i+=1 
     return i-1
+
 
 def length(nString:str): 
     nMark : chr = '.'
@@ -49,13 +53,22 @@ def initializeArray (array : list, n :int, m:int, array2:list , n2:int) :
         for j in range(m): 
             newArray[i][j] = array[i][j]
     
-    if(array2!="."):
-        for i in range(n2): 
-            for j in range(m): 
-                newArray[i + n][j] = array2[j]
+    for i in range(n2): 
+        for j in range(m): 
+            newArray[i + n][j] = array2[j]
 
     return newArray
-            
+def initializeArray2 (array : list, n :int, m:int, array2:list , n2:int) : 
+    newArray = [["" for i in range(m)] for i in range(n+n2)]
+    for i in range(n):
+        for j in range(m): 
+            newArray[i][j] = array[i][j]
+    
+    for i in range(n2): 
+        for j in range(m): 
+            newArray[i + n][j] = array2[i][j]
+
+    return newArray           
         
 def splitArr(nString:str,n:int): #Split dilakukan dengan asumsi ";" sebagai pemisah
     tmpArray = ["" for i in range(n)]
@@ -96,7 +109,7 @@ def load(namaFile:str,n:int) :  #Prosedur load
     isiFile= f.readline()
     isiFile = str(isiFile)
     x = 0 
-    tmpArray = [[""for i in range(n)]for i in range(panjangArray(namaFile))]
+    tmpArray = [[""for i in range(n)]for i in range(panjangFile(namaFile))]
     while(isiFile != ""): 
         tmpStr = splitArr(isiFile,n)
         for i in range(n) : 
@@ -150,11 +163,23 @@ def switch(userCommand):
         printUser()
     elif(userCommand == "hapusjin"):
         hapusJin()
+    elif(userCommand == "save"): 
+        save()
+    elif(userCommand == "ubahjin"): 
+        ubahTipeJin()
+    elif(userCommand == "ayamberkokok"): 
+        ayamBerkokok()
+    elif(userCommand == "kumpul"): 
+        Kumpul()
+    elif(userCommand == "bangun"): 
+        Bangun()
+    elif(userCommand == "exit"): 
+        Exit()
 
 def logIn(): 
     print("Silahkan masukkan username Anda")
     global userName , userPass
-    lengthArray = panjangArray("user.csv")
+    lengthArray = panjangFile("user.csv")
     while True: 
         userName = input(">>>> ")
         if(cekUser(userName,userFile, lengthArray)) : 
@@ -186,7 +211,7 @@ def summonJin():
             print('Tidak ada jenis jin bernomor "%d"' %inputNomer)
     while True: 
         namaJin = input("Masukkan username jin : ")
-        if(cekUser(namaJin, userFile,panjangArray("user.csv"))) : 
+        if(cekUser(namaJin, userFile,panjangFile("user.csv"))) : 
             print('Username "Genie" sudah diambil! ')
         else: 
             print("Pendaftaran Username berhasil")
@@ -202,14 +227,82 @@ def summonJin():
             break
     print("Jin " + namaJin + " berhasil dipanggil")
     arrayTemp = [namaJin, passJin, tipeJin]
-    arrayNew = initializeArray(userFile, panjangArray("user.csv"),3,arrayTemp,1)
-    writeFile("user.csv",arrayNew,panjangArray("user.csv")+1,3)
-    load()
+    arrayNew = initializeArray(userFile, panjangFile("user.csv"),3,arrayTemp,1)
+    writeFile("user.csv",arrayNew,panjangFile("user.csv")+1,3)
+    load("user.csv",3)
 
+def lcg(a : int , c: int, m: int ) -> int : # X = (a*x + c) mod m 
+    global seed 
+    hasilLCG = (a*seed + c) % m
+    seed = hasilLCG
+    return hasilLCG %6
+
+def save(): 
+    panjangFileUser = panjangFile("user.csv")
+    panjangFileBahan = panjangFile("bahan_bangunan.csv")
+    panjangFIleCandi = panjangFile("candi.csv")
+    namaFolder = input("Masukklan nama folder : ")
+    print("Saving...")
+    if not os.path.exists("save"): 
+        os.mkdir("save")
+
+    namaFolder= "save/"+namaFolder
+    if os.path.exists(namaFolder): 
+        os.chdir(namaFolder)
+    else : 
+        print("Membuat folder " + namaFolder)
+        os.mkdir(namaFolder)
+        os.chdir(namaFolder)
+    writeFile("user.csv", userFile , panjangFileUser,3 )
+    writeFile("bahan_bangunan.csv",bahanBangunan, panjangFileBahan,3 )
+    writeFile("candi.csv", daftarCandi, panjangFIleCandi,5 )
+    os.chdir("../")   #Keluar dari folder save
+    os.chdir("../")   #Keluar dari folder parent save
+    print("Berhasil menyimpan data di folder " + namaFolder + " !")    
+
+def getJin(userName): 
+    arrayJin = ["" for i in range(3)]
+    for i in range(panjangFile("user.csv")): 
+        for j in range(3): 
+            if(userFile[i][0] == userName ): 
+                arrayJin = [i]
+    return arrayJin
+ 
+def ubahTipeJin():
+    while True  : 
+        userNameJin = input("Masukkan username jin : ")
+        if( cekUser(userNameJin)) : 
+            break 
+        else : 
+            print("Tidak ada jin dengan username tersebut.")
+    arrayJin = getJin(userNameJin)
+    while True :
+        if(arrayJin[2] == "Pengumpul"): 
+            konfirmasiUser = input('Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ')
+        elif(arrayJin[2] == "Pembangung"): 
+            konfirmasiUser = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ')
+        else : 
+            print("Error")
+            return 0
+        if(konfirmasiUser == "Y" or konfirmasiUser =="y"): 
+            break
+        else : 
+            return 0 
+        
+    for i in range(panjangFile("user.csv")):
+        if(userFile[i][0] == userNameJin):
+            if(arrayJin[2] == "Pembangun"): 
+                userFile[i][2] = "Pengumpul"
+            else : 
+                userFile[i][2] ="Pembangun"
+    writeFile("user.csv", userFile,panjangFile("user.csv"), 3)
+    print("Tipe jin berhasil diubah")
+    return 0 
+    
 def hapusJin(): 
     while True : 
         namaJin = input("Masukkan username jin : ")
-        if( cekUser(namaJin,userFile,panjangArray("user.csv"))) : 
+        if( cekUser(namaJin,userFile,panjangFile("user.csv"))) : 
             break 
         else : 
             print("Tidak ada jin dengan username tersebut.")
@@ -219,35 +312,85 @@ def hapusJin():
             break
         else : 
             return 0
-    arrayNew = [["" for i in range(3)] for i in range(panjangArray("user.csv") -1)]
+    arrayNew = [["" for i in range(3)] for i in range(panjangFile("user.csv") -1)]
     x = 0
-    for i in range(panjangArray("user.csv")):
+    for i in range(panjangFile("user.csv")):
         for j in range(3): 
             if(userFile[i][0] != namaJin):
                 arrayNew[i][j] = userFile[x][j]
         x+=1
-    writeFile("user.csv", arrayNew, panjangArray("user.csv")-1 ,3)
+    writeFile("user.csv", arrayNew, panjangFile("user.csv")-1 ,3)
     print("Jin "+ namaJin + " berhasil dihapus.")
-    load()
+    load("user.csv",3)
     
+def ayamBerkokok(): 
+    print("Kukuruyuk.. Kukuruyuk..\n")
+    print("Jumlah candi : " + str(panjangFile("candi.csv") -1) +"\n")
+    if(panjangFile("candi.csv") -1 == 100): 
+        print("Yah, Bandung Bondowoso memenangkan permainan!")
+    else : 
+        
+        print("Selamat, Roro Jonggrang memenangkan permainan!")
+        print("\n")
+        print("*Bandung Bondowoso angry noise* \nRoro Jonggrang dikutuk menjadi candi.")
 
+def Kumpul(): 
+    global totalBatu, totalPasir, totalAir, bahanBangunan
+    nPasir = lcg(151811,3112,50603)
+    nBatu =lcg(151813,3112,50603)
+    nAir = lcg(151817,3112,50603)
+    totalBatu += nBatu
+    totalAir += nAir
+    totalPasir += nPasir
+    print("Jin menemukan " + str(nPasir) + " pasir "  + str(nBatu) + " batu " + str(nAir) + " air")
+    arrayTemp = [["Pasir","sebuah pasir",str(nPasir)], ["Batu","sebuah batu", str(nBatu)], ["Air", "sebuah air", str(nAir)]]
+    if(panjangFile("bahan_bangunan.csv") == 1): 
+        arrayNew = initializeArray2(bahanBangunan,panjangFile("bahan_bangunan.csv"), 3, arrayTemp, 3)
+    else : 
+        bahanBangunan[1][2] += nPasir
+        bahanBangunan[2][2] += nBatu
+        bahanBangunan[3][2] += nAir
+    print(arrayNew)
+    writeFile("bahan_bangunan.csv", arrayNew, panjangFile("bahan_bangunan.csv")+3, 3)
+    load("bahan_bangunan.csv",3)
+def Bangun(): 
+    global totalPasir, totalBatu, totalAir
+    nPasir = lcg(151873,3112,50603)
+    nBatu =lcg(151879,3112,50603)
+    nAir = lcg(151837,3112,50603)
+    print("Bahan yang diperlukan : " + str(nPasir) + " pasir, " + str(nBatu) + " batu, " + str(nAir) +" air")
+    if(nPasir <= totalPasir and nAir <= totalAir and nBatu <= totalBatu): 
+        print("Candi berhasil dibangun.")
+        sisaCandi =100 - panjangFile("candi.csv")
+        print("Sisa candi yang perlu dibangun: " + str(sisaCandi))
+        totalPasir -= nPasir
+        totalAir -= nAir
+        totalBatu -= nBatu
+    else  : 
+        print("Bahan bangunan tidak mencukupi")
+        print("Candi tidak bisa dibangun!")
+def Exit(): 
+    panjangFileUser = panjangFile("user.csv")
+    panjangFileBahan = panjangFile("bahan_bangunan.csv")
+    panjangFIleCandi = panjangFile("candi.csv")
+    while True :
+        konfirmasiUser = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+        if(konfirmasiUser == "Y" or konfirmasiUser =="y"): 
+            writeFile("user.csv", userFile , panjangFileUser,3 )
+            writeFile("bahan_bangunan.csv",bahanBangunan, panjangFileBahan,3 )
+            writeFile("candi.csv", daftarCandi, panjangFIleCandi,5 )
+            break; 
+        else : 
+            break
+    sys.exit()
 #Program Utama
 memintaArgs()
 print("Loading...")
 print('Selamat datang di program "Manajerial Candi" ')
- 
+
+userFile = load("user.csv",3)
+daftarCandi = load("candi.csv",5)
+bahanBangunan = load("bahan_bangunan.csv",3)
 while sedangBerjalan : 
     x = input(">>>> ")
     switch(x) 
-
-print(userFile)
-
-
-
-
-
-
-
-
-
-
